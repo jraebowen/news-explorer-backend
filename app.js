ab8731c967b759195d9b2c749af65217b48a4077
Jaimie Bowen <jraebowen@gmail.com>
2025-12-01 11:02:29 -0800

    update jwt secret and move to .env file. validation WIP

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const { errors } = require("celebrate");
+require("dotenv").config();
 
 const app = express();
 const { PORT = 3002 } = process.env;
+
+mongoose.connect("mongodb://127.0.0.1:27017/newsexplorer_db");
 
 const mainRouter = require("./routes/index.js");
 const errorHandler = require("./middlewares/error-handler");
 
 app.use(express.json());
-express.urlencoded({ extended: true });
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
 app.use("/", mainRouter);
 
+app.use(errors()); // celebrate error handler
 app.use(errorHandler);
-
-mongoose.connect("mongodb://127.0.0.1:27017/newsexplorer_db");
 
 app.listen(PORT, () => {
   // if everything works fine, the console will show which port the application is listening to
